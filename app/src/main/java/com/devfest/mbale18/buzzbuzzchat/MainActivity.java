364ad6ff0d19107b7540de553e2f34323c4c0693
codephillip <codephillip@gmail.com>
2018-11-03 10:07:10 +0300

    integrated firebase, added get, post data and realtime fetching

--- app/src/main/java/com/devfest/mbale18/buzzbuzzchat/MainActivity.java
+++ app/src/main/java/com/devfest/mbale18/buzzbuzzchat/MainActivity.java
@@ -1,13 +1,108 @@
- package com.devfest.mbale18.buzzbuzzchat;
+package com.devfest.mbale18.buzzbuzzchat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private FirebaseFirestore db;
+    private static final String CHAT = "chat";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = FirebaseFirestore.getInstance();
+
+        try {
+            saveDocumentData();
+            readDocumentData();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        db.collection(CHAT)
+//                .whereEqualTo("name", "Phillip")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "listen:error", e);
+                            return;
+                        }
+
+                        for (QueryDocumentSnapshot snapshot : queryDocumentSnapshots) {
+                            if (snapshot != null && snapshot.exists()) {
+                                Log.d(TAG, "Current data: " + snapshot.getData());
+                            } else {
+                                Log.d(TAG, "Current data: null");
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void saveDocumentData() {
+        Log.d(TAG, "saveDocumentData: started");
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", "Sharon");
+        user.put("message", "How are you");
+
+        db.collection(CHAT)
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
+    private void readDocumentData() {
+        Log.d(TAG, "readDocumentData: started");
+        db.collection(CHAT)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 }
